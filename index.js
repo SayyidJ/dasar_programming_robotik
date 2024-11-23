fac1e0f196f2af6ddf59144aba52508cd3701ba7
SayyidJ <sayyidjee@gmail.com>
2024-11-23 12:34:12 +0700

    make a router

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-// <!DOCTYPE html>
-// <html lang="en">
-//   <head>
-//     <meta charset="UTF-8" />
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-//     <title>Redirecting...</title>
-//     <script>
-//       window.location.href = "/build/index.html";
-//     </script>
-//   </head>
-//   <body>
-//     <p><a href="/build/index.html">click here</a>.</p>
-//   </body>
-// </html>
-
-window.location.href = "/build/index.html";
+const path = window.location.pathname;
+if (path === "/") {
+  window.location.href = "/build/";
+} else {
+  window.location.href = `/build${path}`;
+}
