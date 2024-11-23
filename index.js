caae9da7c188364944b00cec4c396125cfef4ba6
SayyidJ <sayyidjee@gmail.com>
2024-11-23 12:37:01 +0700

    up

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const path = window.location.pathname;
-if (path === "/") {
-  window.location.href = "/build/";
-} else {
-  window.location.href = `/build${path}`;
-}
+window.location.href = "/build/";
